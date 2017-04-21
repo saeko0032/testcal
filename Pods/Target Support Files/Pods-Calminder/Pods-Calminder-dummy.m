e6866ea4d88466193c08de234605c55a72e525da
Saeko Fukui <saekof@CCL107iM04.local>
2017-04-20 21:16:22 -0700

    test

---
+++ Pods/Target Support Files/Pods-Calminder/Pods-Calminder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Calminder : NSObject
+@end
+@implementation PodsDummy_Pods_Calminder
+@end
---
+++ Pods/Target Support Files/Pods-CalminderTests/Pods-CalminderTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CalminderTests : NSObject
+@end
+@implementation PodsDummy_Pods_CalminderTests
+@end
